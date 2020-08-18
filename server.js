e17a5643a0cb2e23843c13a735acf69438f4f536
stavroslee <stavros.lee@gmail.com>
2020-08-18 14:33:28 -0700

    Update server.js
    change server to support post for /token which client is expecting.
    pulled out helper function
    
    added body parser to get json body params

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 require('dotenv').config();
@@ -11,17 +12,28 @@ const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
 app.use(express.static(path.join(__dirname, 'build')));
+let jsonParser = bodyParser.json()
 
-app.get('/token', (req, res) => {
-  const { identity, roomName } = req.query;
+app.post('/token', jsonParser, (req, res) => {
+  const { user_identity:identity, room_name:roomName } = req.body;
+  res.send(getToken(identity, roomName));
+});
+
+function getToken(identity, roomName) {
   const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
     ttl: MAX_ALLOWED_SESSION_DURATION,
   });
   token.identity = identity;
   const videoGrant = new VideoGrant({ room: roomName });
   token.addGrant(videoGrant);
-  res.send(token.toJwt());
   console.log(`issued token for ${identity} in room ${roomName}`);
+  return JSON.stringify({token:token.toJwt()});  
+}
+
+app.get('/token', (req, res) => {
+  const { identity, roomName } = req.query;
+  let jwt = getToken(identity, roomName);
+  res.send(jwt);
 });
 
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
